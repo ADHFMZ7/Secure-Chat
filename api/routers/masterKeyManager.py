60f9234cd2005b445861d3d940ac6be9bae0e1d1
smminassian <sebastienminassian203@gmail.com>
2024-12-19 20:45:20 -0800

    Im starting to add my key distribution stuff. The first time the application runs, we will generate a kdc public key and kdc private key

--- api/routers/masterKeyManager.py
+++ api/routers/masterKeyManager.py
@@ -1,16 +1,39 @@
 from Cryptodome.PublicKey import RSA
 from Cryptodome.Cipher import PKCS1_OAEP
 from aiosqlite import connect
+from fastapi import APIRouter, HTTPException, Depends, Form
+from dependencies import get_db
+import os
 
-async def decrypt_and_store_master_key(user_id: int, master_key: str, kdc_public_key_path: str, database_url: str):
-    #Load with  KDC's 
+#Generate KDC's private key
+router = APIRouter()
+
+def generate_kdc_private_key():
+    if not os.path("kdc_private.pem"):
+        KDC_private_key = RSA.generate(1024)
+        kdcPrivKeyBytes = KDC_private_key.export_key(format="PEM")
+        with open ("kdc_private.pem", "wb") as kdc_priv:
+            kdc_priv.write(kdcPrivKeyBytes)
+        
+    else:
+        print(f"priv key already exists")
+
+def generate_kdc_public_key():
+    if not os.path("kdc_public.pem"):
+        KDC_public_key = RSA.generate(1024)
+        kdcPublicBytes = KDC_public_key.export_key(format = "PEM")
+        with open("kdc_public.pem", "wb") as kdc_pub:
+            kdc_pub.write(kdcPublicBytes)
+    else:
+        print(f"pub key already exists")
 
 
 
 
-    with open(kdc_public_key_path, "rb") as key_file:
-        kdc_public_key = RSA.import_key(key_file.read())
-    
-    #Encrypt the master key using KDC's public key
+@router.on_event("startup")
+async def startup_event():
+    """Ensure the private key and public key is available at application start up"""
+    generate_kdc_private_key()
+    generate_kdc_public_key()
 
-    cipher_rsa = 
+
